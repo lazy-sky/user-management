53c69af092aebd709971ea56379fc2c4acedee94
lazy-sky <1cielo@naver.com>
2022-10-16 08:11:34 +0900

    Feat: Declare necessary module types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,3 +2,14 @@ declare module '*.scss' {
   const content: { [className: string]: string }
   export = content
 }
+
+declare module '*.png'
+
+declare module '*.ttf'
+declare module '*.svg' {
+  import React = require('react')
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
+  const src: string
+  export default src
+}
