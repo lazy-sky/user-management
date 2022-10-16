b71d46876ada0fa08d8d5163a2033e01d6a356d8
lazy-sky <1cielo@naver.com>
2022-10-16 21:47:42 +0900

    Feat: Make SaveButton

---
+++ src/components/SaveButton.tsx
@@ -0,0 +1,43 @@
+import { useSelector } from 'react-redux'
+import styled from 'styled-components'
+
+import { toggleUserChecked } from 'services/data'
+import { getUsers } from 'states/users'
+
+const ButtonWrapper = styled.div`
+  padding: 20px;
+  background: ${({ theme }) => theme.colors.white};
+
+  button {
+    width: 100%;
+    padding: 8px;
+    color: ${({ theme }) => theme.colors.white};
+    background: ${({ theme }) => theme.colors.blue};
+    font-weight: 300;
+    font-size: 15px;
+    line-height: 19px;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+`
+
+const SaveButton = () => {
+  const users = useSelector(getUsers)
+
+  const handleSaveClick = () => {
+    for (const user of users) {
+      toggleUserChecked(user.id, user.checked)
+    }
+  }
+  return (
+    <ButtonWrapper>
+      <button type='button' onClick={handleSaveClick} className='saveBtn'>
+        저장하기
+      </button>
+    </ButtonWrapper>
+  )
+}
+
+export default SaveButton
